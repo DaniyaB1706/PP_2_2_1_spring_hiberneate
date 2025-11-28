10c1debc0015e6fc085227402478a39161e0493d
Daniya B <daniyab1706@gmail.com>
2025-11-28 21:05:51 +0500

    Completed PP_2.2.1 Spring Hibernate task

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,8 +1,9 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
@@ -11,19 +12,37 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory
+                .getCurrentSession()
+                .createQuery("from User", User.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public User findUserByCar(String model, int series) {
+        Session session = sessionFactory.getCurrentSession();
+
+        String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+
+        TypedQuery<User> query = session.createQuery(hql, User.class);
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+
+        List<User> list = query.getResultList();
+
+        return list.isEmpty() ? null : list.get(0);
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -4,26 +4,29 @@ import hiber.dao.UserDao;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
 
+    @Override
+    public User findUserByCar(String model, int series) {
+        return userDao.findUserByCar(model, series);
+    }
 }
